04c4440af4fc47d09b4236a78f3620b0ad426e50
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-20 21:51:59 -0600

    define constants

---
+++ index.js
@@ -0,0 +1,5 @@
+const productRoutes = require("./routes/products.routes");
+const express = require('express');
+const cors = require('cors');
+const { AppDataSource } = require("./data/source");
+const PORT = 3000;
