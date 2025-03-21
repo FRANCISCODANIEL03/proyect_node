0e93197ee556773aeeb0275874cb458273259ca7
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-20 21:52:20 -0600

    add middlewares

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const express = require('express');
 const cors = require('cors');
 const { AppDataSource } = require("./data/source");
 const PORT = 3000;
+
+const app = express();
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
