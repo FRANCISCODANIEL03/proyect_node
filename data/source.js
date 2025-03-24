8d903a06f961a404a3442ff1c81eb88139631151
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-24 01:29:25 -0600

    define AppDataSource

--- data/source.js
+++ data/source.js
@@ -3,3 +3,15 @@ const { Product } = require("../models/Products");
 const dotenv = require('dotenv');
 dotenv.config()
 require("reflect-metadata");
+
+const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    entities: [Product],
+});
+
+module.exports = { AppDataSource }
