271a671c9b02fbf71b98aa9131cf7616d35eba03
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-23 22:03:20 -0600

    add new functions

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -10,6 +10,21 @@ class ProductController {
             return res.status(500).json({ message: error.message || "Error interno al leer los datos" });
         }
     }
+    static async getOneById(req, res) {
+        try {
+            const { id } = req.params;
+            const product = await ProductService.getOneBy(id);
+            if (!product) {
+                return res.status(404).json({ message: "Producto no existente" });
+            }
+            if (!id || isNaN(id)) {
+                return res.status(400).json({ message: "Ingresa in id valido" });
+            }
+            return res.status(200).json(product);
+        } catch (error) {
+            return res.status(500).json({ message: error.message || "Error interno al leer los datos" });
+        }
+    }
 
 }
 
