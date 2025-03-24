a62aafd8003ca274459cdd5ff60f216a4c83c028
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-23 22:03:08 -0600

    add new function

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,6 +2,14 @@ const { ProductService } = require("../service/products.service")
 const { createProduct } = require("../functions/validate")
 
 class ProductController {
+    static async getAll(req, res) {
+        try {
+            const products = await ProductService.getAll();
+            return res.status(200).json(products);
+        } catch (error) {
+            return res.status(500).json({ message: error.message || "Error interno al leer los datos" });
+        }
+    }
 
 }
 
