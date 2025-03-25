c684ab46aa8923b56c92bdd1de1d34d9ec948b94
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-24 19:54:44 -0600

    add function to validate the update of products

--- functions/validate.js
+++ functions/validate.js
@@ -12,3 +12,22 @@ function createProduct(data) {
   return true;
 }
 
+function updateProduct(data) {
+  // Validaciones de datos
+  if (data.nombre && typeof data.nombre !== "string") {
+    return false
+  }
+
+  if (data.precio && (isNaN(data.precio) || data.precio <= 0)) {
+    return false
+  }
+
+  if (data.stock && (!Number.isInteger(data.stock) || data.stock < 0)) {
+    return false
+  }
+  return true
+}
+
+module.exports = { createProduct, updateProduct };
+
+
