15452e11411f67381eb8c91914bf36571a0417ed
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-26 20:07:42 -0600

    add new function

--- service/products.service.js
+++ service/products.service.js
@@ -1,6 +1,10 @@
 const { ProductRepository } = require("../repositories/product.repository");
 
 class ProductService{
-   
+    async getAll(){
+        return await ProductRepository.find();
+    }
+
+    
 }
 
