55a7dbc62c73c3e811181206d1b8595b2fe56b6a
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-03-27 20:10:52 -0600

    add new entity

--- models/Products.js
+++ models/Products.js
@@ -1,2 +1,28 @@
 const { EntitySchema } = require("typeorm");
 
+const Product = new EntitySchema({
+    name: "Product",
+    tableName: "productos",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        nombre: {
+            type: "varchar",
+            length: 255,
+            nullable: false,
+        },  
+        precio: {
+            type: "decimal",
+            precision: 10,
+            scale: 2,
+        },
+        stock: {
+            type: "int",
+        },
+    },
+});
+
+module.exports = { Product };
